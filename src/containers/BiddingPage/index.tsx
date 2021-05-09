c71274a6b7d270d4854c43072b3ccedcc6b72eea
Shiva Krishna Gopannagari <gopannagarishiva@gmail.com>
2021-05-09 16:26:47 +0530

    Title with divider added

--- src/containers/BiddingPage/index.tsx
+++ src/containers/BiddingPage/index.tsx
@@ -5,5 +5,9 @@ import './index.scss';
 export const BiddingPage: FC = () => {
     return <div id="biddingPage">
         <Header />
+        <div className="title">
+            <div className="content">Old Car Available</div>
+            <div className="titleDivider" />
+        </div>
     </div>
 }
