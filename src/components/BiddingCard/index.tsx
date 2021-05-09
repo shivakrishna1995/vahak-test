ca863f6018c6db16c673df256ee55a7387b1d1d1
Shiva Krishna Gopannagari <gopannagarishiva@gmail.com>
2021-05-09 19:50:55 +0530

    Form header added

--- src/components/BiddingCard/index.tsx
+++ src/components/BiddingCard/index.tsx
@@ -3,6 +3,18 @@ import './index.scss'
 
 export const BiddingCard: FC = () => {
     return <div id="biddingCard">
-        
+        <div className="biddingCardHeader">
+            <div className="biddingCardHeaderTitle">Place Your Load BID</div>
+            <div className="divider" />
+            <div className="biddingCardAmount">
+                <div className="biddingCardAmountSymbol">₹</div>
+                <div className="biddingCardAmountContent">2,50,000</div>
+            </div>
+            <div className="biddingCardHeaderActions">
+                <div className="biddingCardHeaderActionsActive">Fixed Price</div>
+                <div className="biddingCardHeaderActionsInactive">Rate Negiotable</div>
+            </div>
+        </div>
+        <div className="biddingCardHorizontalLine"/>
     </div>
 }
